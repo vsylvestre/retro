d2ed2dd26c7b61bb63e3e9e00c06e33ba4cd96f1
Vincent Sylvestre <vinc.sylvestre@gmail.com>
2020-06-12 15:55:14 -0400

    Fix express config to support Room URLs
    Signed-off-by: Vincent Sylvestre <vinc.sylvestre@gmail.com>

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function launch() {
 
     if (process.env.NODE_ENV === "production") {
         app.use(express.static(path.join(__dirname, 'build')));
-        app.get('/', function(req, res) {
+        app.get('/*', function(req, res) {
             res.sendFile(path.join(__dirname, 'build', 'index.html'));
         });
     }
